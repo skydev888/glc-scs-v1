1a442a9d0d66e5ce2d55103f1ddbf9b6945e81e9
ysdev888 <y.s.1@icloud.com>
2019-12-01 10:27:55 +0700

    branch3-4

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -53,11 +53,11 @@ urlpatterns += [
 ]
 
 
-
-#Add Django site authentication urls (for login, logout, password management)
-urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
-]
+#
+# #Add Django site authentication urls (for login, logout, password management)
+# urlpatterns += [
+#     path('accounts/', include('django.contrib.auth.urls')),
+# ]
 
 # 追加：ユーザー登録アプリ
 urlpatterns += [
